64ebda3fefbd80af4e09085f8b6575c87d989b83
hltnfagner-spec <hltnfagner@gmail.com>
2026-04-17 20:19:27 -0300

    Update SobreMim.jsx

--- src/components/SobreMim.jsx
+++ src/components/SobreMim.jsx
@@ -5,12 +5,12 @@ function SobreMim() {
       <h2>Sobre Mim</h2>
       <div className="sobre-container">
         <div className="foto-perfil">
-          {/* A foto agora precisa ficar na pasta raiz chamada public, ou deixamos sem por enquanto pra focar na nota */}
-          <img src="https://github.com/hltnfagner-spec.png" alt="Foto de Hilton Fagner" />
+          {/* Pegando a minha foto lá da pasta public (o Vite entende que a barra / vai direto pra lá) */}
+          <img src="/minha-foto.JPG" alt="Foto de Hilton Fagner" />
         </div>
         <div className="sobre-texto">
           <p>Sou <strong>Hilton Fagner</strong>, desenvolvedor de sistemas em formação e moro na cidade de Domingos Mourão. Atualmente, possuo formação Técnica em Zootecnia e pretendo trabalhar no desenvolvimento de soluções para pequenos produtores rurais.</p>
-          <p>Meu foco atual é criar sistemas descomplicados para gerenciar a rentabilidade de projetos locais, em especial iniciativas voltadas para a apicultura na nossa região, unindo o conhecimento de campo com a tecnologia.</p>
+          <p>Meu objetivo é facilitar o acesso a sistemas descomplicados que permitam gerenciar índices zootécnicos e a rentabilidade do negócio, unindo o conhecimento de campo com a tecnologia.</p>
         </div>
       </div>
     </section>
