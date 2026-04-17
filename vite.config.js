a72191944994c25fbe62ff131ccfc561a412a186
hltnfagner-spec <hltnfagner@gmail.com>
2026-04-17 19:58:22 -0300

    Add Vite configuration for React project

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Configuração padrão pro React rodar liso
+export default defineConfig({
+  plugins: [react()],
+})
